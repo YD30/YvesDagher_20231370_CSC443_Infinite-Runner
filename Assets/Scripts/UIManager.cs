4c92dd366fbd031b65d85e866c713b1ab945df66
YD30 <79518115+YD30@users.noreply.github.com>
2026-05-11 17:05:31 +0300

    Score added
    Score now appear in Gameplay and gameOver

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject GameOverPanel;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI finalText;
 
     private bool shown = false;
 
      void Update()
     {
+        if (GameOverPanel == null) return;
+         float score = GameManager.Instance.Distance;
+         scoreText.text = $"Score: {score:0}";
+
         if (GameManager.Instance.IsGameOver && !shown)
         {
             Debug.Log("GAME OVER DETECTED");
             GameOverPanel.SetActive(true);
             shown = true;
+            finalText.text = $"Final Score: {score:0}";
         }
     }
 
